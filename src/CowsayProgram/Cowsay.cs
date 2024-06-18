562ea28a893e709ca96953287da6e86c79f8d257
windss <tamayowendell05@gmail.com>
2024-06-18 21:51:14 +0800

    chore: refactored code

--- src/CowsayProgram/Cowsay.cs
+++ src/CowsayProgram/Cowsay.cs
@@ -1,34 +1,32 @@
 using System.Diagnostics;
 
-namespace CowsayProgram
+namespace CowsayProgram;
+public class Cowsay
 {
-    public class Cowsay
+    public event EventHandler<string>? Reply;
+
+    private static Process InitCowsay() => new()
     {
-        public event EventHandler<string>? Reply;
-
-        private static Process InitCowsay() => new()
+        StartInfo = new ProcessStartInfo()
         {
-            StartInfo = new ProcessStartInfo()
-            {
-                FileName = "cowsay",
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false
-            }
-        };
-
-        public void Say(string message)
-        {
-            Process cowsay = InitCowsay();
-            cowsay.Start();
-            
-            cowsay.StandardInput.WriteLine(message);
-            cowsay.StandardInput.Close();
-
-            string output = cowsay.StandardOutput.ReadToEnd();
-            cowsay.WaitForExit();
-
-            Reply?.Invoke(this, output);
+            FileName = "cowsay",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false
         }
+    };
+
+    public void Say(string message)
+    {
+        Process cowsay = InitCowsay();
+        cowsay.Start();
+
+        cowsay.StandardInput.WriteLine(message);
+        cowsay.StandardInput.Close();
+
+        string output = cowsay.StandardOutput.ReadToEnd();
+        cowsay.WaitForExit();
+
+        Reply?.Invoke(this, output);
     }
 }
